04a48cd64326407c4b98e644bc51fa4deca09103
Jacob van Mourik <jacob.v.m@gmail.com>
2015-08-24 23:24:25 +0200

    Bumped version to 0.2.2

--- src/main/java/com/jvms/i18neditor/Editor.java
+++ src/main/java/com/jvms/i18neditor/Editor.java
@@ -52,7 +52,7 @@ public class Editor extends JFrame {
 	
 	public static final Path SETTINGS_PATH = Paths.get(System.getProperty("user.home"), ".i18n-editor");
 	public static final String TITLE = "i18n Editor";
-	public static final String VERSION = "0.2.1";
+	public static final String VERSION = "0.2.2";
 	public static final int DEFAULT_WIDTH = 1024;
 	public static final int DEFAULT_HEIGHT = 768;
 	
